491ea3b5c93738e328348a56c2c07316eec898c2
morgen-montgomery <mfett004@gmail.com>
2016-05-05 11:49:42 -0700

    camera shop done

---
+++ script.js
@@ -0,0 +1,30 @@
+module.exports = {
+  sum: function(){
+
+  },
+  isEqual: function(){
+
+  },
+  isEven: function(){
+
+  },
+  isDivisible: function(){
+
+  },
+  discountPercentage: function(){
+
+  },
+  isVowel: function(){
+
+  },
+  celsiusToFahrenheit: function(){
+
+  },
+  biggestOfThree: function(){
+
+  },
+  // Bonus
+  federalIncomeTaxCalculator: function(){
+
+  }
+};
